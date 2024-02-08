6245c40f1832e6d38895e1a0c5eea445b152a1d1
Kate <katshe18@gmail.com>
2024-02-08 14:28:49 +1300

    used v-bind to link the image dynamically from data()

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       description: "Some socks.",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://www.vuemastery.com/",
     };
   },
 });
