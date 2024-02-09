00eba627eed435ceeb5688a232267a036515bcb6
Kate <katshe18@gmail.com>
2024-02-10 07:47:15 +1300

    Added arrays to the data object and rendered these as lists

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       image: "./assets/images/socks_blue.jpg",
-      inventory: 100,
-      onSale: true,
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: ["XS", "S", "M", "L", "XL"],
     };
   },
 });
