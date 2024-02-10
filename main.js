50efe85a12a147edef2dc66647de5d856e7542dc
Kate <katshe18@gmail.com>
2024-02-10 15:17:34 +1300

    Class & style binding based on product in stock

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
+      brand: "Vue Mastery",
       image: "./assets/images/socks_blue.jpg",
-      inStock: true,
+      inStock: false,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
-      sizes: ["XS", "S", "M", "L", "XL"],
     };
   },
-
   methods: {
     addToCart() {
       this.cart += 1;
